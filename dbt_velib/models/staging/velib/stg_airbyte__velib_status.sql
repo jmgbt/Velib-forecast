3762e6a66b97f06975e28e49fe20903e423b8638
Erik Owens <erikrowens@gmail.com>
2024-11-06 10:12:27 +0100

    modified  stg_airbyte__station_status.sql to ingore when mech and ebike are both NULL

--- dbt_velib/models/staging/velib/stg_airbyte__velib_status.sql
+++ dbt_velib/models/staging/velib/stg_airbyte__velib_status.sql
@@ -1,27 +1,31 @@
 -- stg_airbyte__station_status.sql
 
-select
- station_id
- , TIMESTAMP_SECONDS(CAST(last_reported as INT64)) as last_reported
-, TIMESTAMP_TRUNC(
-    TIMESTAMP_SUB(
-        TIMESTAMP_SECONDS(CAST(last_reported as INT64)),
-        INTERVAL MOD(EXTRACT(MINUTE FROM TIMESTAMP_SECONDS(CAST(last_reported as INT64))), 10) MINUTE
-    ),
-    MINUTE
-) AS last_reported_1O
+SELECT *
+FROM (
+  SELECT
+  station_id
+  , TIMESTAMP_SECONDS(CAST(last_reported as INT64)) AS last_reported
+  , TIMESTAMP_TRUNC(
+      TIMESTAMP_SUB(
+          TIMESTAMP_SECONDS(CAST(last_reported as INT64)),
+          INTERVAL MOD(EXTRACT(MINUTE FROM TIMESTAMP_SECONDS(CAST(last_reported as INT64))), 10) MINUTE
+      ),
+      MINUTE
+  ) AS last_reported_1O
 
- , num_docks_available
+  , num_docks_available
 
-  ,CAST(JSON_EXTRACT_SCALAR(
-    JSON_EXTRACT_ARRAY(num_bikes_available_types)[OFFSET(0)],
-    '$.mechanical'
-  ) AS INT) as mechanical_bikes_available,
+    ,CAST(JSON_EXTRACT_SCALAR(
+      JSON_EXTRACT_ARRAY(num_bikes_available_types)[OFFSET(0)],
+      '$.mechanical'
+    ) AS INT) AS mechanical_bikes_available,
 
-  CAST(JSON_EXTRACT_SCALAR(
-    JSON_EXTRACT_ARRAY(num_bikes_available_types)[OFFSET(1)],
-    '$.ebike'
-  ) AS INT) as ebikes_available
- , is_installed,
-  is_renting, is_returning
- from {{ source('Airbyte','velib_status') }}
+    CAST(JSON_EXTRACT_SCALAR(
+      JSON_EXTRACT_ARRAY(num_bikes_available_types)[OFFSET(1)],
+      '$.ebike'
+    ) AS INT) AS ebikes_available
+  , is_installed,
+    is_renting, is_returning
+  FROM {{ source('Airbyte','velib_status') }}
+)
+WHERE mechanical_bikes_available IS NOT NULL AND ebikes_available IS NOT NULL

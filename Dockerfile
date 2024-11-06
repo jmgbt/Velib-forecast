11bd430aeae841f83919562b2fb37edcd76347f1
Erik Owens <erikrowens@gmail.com>
2024-11-06 10:48:07 +0100

    Emeric Ariflow installed in Erik env

--- Dockerfile
+++ Dockerfile
@@ -1,25 +1,21 @@
-FROM python:3.8.10-slim
-
-#Do not use env as this would persist after the build and would impact your containers, children images
+FROM python:3.9.20-slim
 
 ARG DEBIAN_FRONTEND=noninteractive
 
-# force the stdout and stderr streams to be unbuffered.
 ENV PYTHONUNBUFFERED 1
 
-ENV  AIRFLOW_HOME=/app/airflow
+ENV AIRFLOW_HOME=/app/airflow
+
 WORKDIR $AIRFLOW_HOME
 
-COPY scripts/entrypoint.sh  ./
-RUN chmod +x /app/airflow/entrypoint.sh
+RUN apt-get update && apt-get install curl -y
 
-COPY poetry.lock ./
-COPY pyproject.toml ./
+COPY scripts scripts
+RUN chmod +x scripts/entrypoint.sh
+RUN chmod +x scripts/init_connections.sh
 
-RUN apt-get update \
-    && apt-get -y upgrade \
-    && pip install --no-cache-dir poetry \
-    && poetry install \
-    && apt-get clean \
+COPY pyproject.toml poetry.lock ./
 
-ENTRYPOINT ["poetry", "run" ]
+RUN pip3 install --upgrade --no-cache-dir pip \
+    && pip3 install poetry \
+    && poetry install --only main
--- dags/dags_velib.py
+++ dags/dags_velib.py
@@ -4,18 +4,108 @@ import pendulum
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 from pathlib import Path
-from app.fetch_data import fetch_and_save_velib_data
-from app.fetch_station_information import fetch_and_save_station_information
-from app.trigger_airbyte import trigger_airbyte
+import requests
+import json
+import requests
+import time
+
 
 AIRFLOW_HOME = os.getenv("AIRFLOW_HOME")
+
+def fetch_and_save_velib_data():
+    url = "https://velib-metropole-opendata.smovengo.cloud/opendata/Velib_Metropole/station_status.json"
+
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        data = response.json()
+
+        # Create a timestamp for the filename
+        timestamp = time.strftime("%Y-%m-%d_%H-%M-%S")
+        filename = f"./data_fetch/velib_station_status_{timestamp}.jsonl"
+
+        # Save the JSON data to a file
+        with open(filename, 'w') as f:
+            for station in data['data']['stations']:
+                f.write(json.dumps(station) + '\n')
+
+def fetch_station_info():
+    url = "https://velib-metropole-opendata.smovengo.cloud/opendata/Velib_Metropole/station_information.json"
+
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        data = response.json()
+
+        # Create a timestamp for the filename
+        timestamp = time.strftime("%Y-%m-%d_%H-%M-%S")
+        filename = f"./data/station_info.jsonl"
+
+        # Save the JSON data to a file
+        with open(filename, 'w') as f:
+            for station in data['data']['stations']:
+                #ignore the rental_methods field - we don't need it and it's a pain
+                if "rental_methods" in station:
+                    del station['rental_methods']
+                f.write(json.dumps(station) + '\n')
+
+def trigger_airbyte():
+    #obtaining the token
+    url = "https://api.airbyte.com/v1/applications/token"
+
+    payload = {
+        "client_id": "91161352-4f8f-4298-a529-9ea628b64c84", #à mettre en os.environ
+        "client_secret": "Az0F0XDLIrNrCppWeYPZtyrRLbaEBDvL", #à mettre en os.environ
+        "grant-type": "client_credentials"
+    }
+    headers = {
+        "accept": "application/json",
+        "content-type": "application/json"
+    }
+    response = requests.post(url, json=payload, headers=headers)
+    token=response.json()['access_token']
+
+    #triggering the job
+
+    url = "https://api.airbyte.com/v1/jobs"
+
+    payload = {
+        "jobType": "sync",
+        "connectionId": "e98c6545-08ae-4c9b-b933-558a8819afa5" #à mettre en os.environ
+    }
+    headers = {
+        "accept": "application/json",
+        "content-type": "application/json",
+        "authorization": f"Bearer {token}"
+    }
+    response = requests.post(url, json=payload, headers=headers)
+    print(response.text)
+
+
+def upload_json_files():
+    """Uploads all JSON files in the current directory to the specified bucket.
+
+    Args:
+        bucket_name: The name of the bucket to upload to.
+    """
+
+    storage_client = storage.Client(project="dataengvelibforecast")  #à mettre en os.environ
+    bucket = storage_client.bucket("velib_status")  #à mettre en os.environ
+
+    velib_status_path ='./data/velib_status'
+    for filename in os.listdir(velib_status_path):
+        if filename.endswith('.jsonl'):
+            full_path = os.path.join(velib_status_path, filename)
+            blob = bucket.blob(filename)
+            blob.upload_from_filename(full_path)
+
 
 
 with DAG(
     "velib_workflow",
     default_args={"depends_on_past": False},
     description="DAG to process velib workflow for DataEng project",
-    schedule_interval='*/10 * * * *', # every 10 minutes
+    schedule_interval=None, #'*/10 * * * *', # every 10 minutes
     catchup = False,
     #depends_on_past=False,
     start_date=pendulum.today("UTC")

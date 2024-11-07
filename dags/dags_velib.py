73a5f6349a31c24f840670890189f195e3e12055
Erik Owens <erikrowens@gmail.com>
2024-11-07 14:28:02 +0100

    minor mod to dbt run dag

--- dags/dags_velib.py
+++ dags/dags_velib.py
@@ -56,39 +56,6 @@ def fetch_station_info():
                 f.write(json.dumps(station) + '\n')
 
 
-# def trigger_airbyte():
-#     #obtaining the token
-#     url = "https://api.airbyte.com/v1/applications/token"
-
-#     payload = {
-#         "client_id": os.getenv("airbyte_client_id"),
-#         "client_secret": os.getenv("airbyte_client_secret"),
-#         "grant-type": "client_credentials"
-#     }
-#     headers = {
-#         "accept": "application/json",
-#         "content-type": "application/json"
-#     }
-#     response = requests.post(url, json=payload, headers=headers)
-#     token=response.json()['access_token']
-
-#     #triggering the job
-
-#     url = "https://api.airbyte.com/v1/jobs"
-
-#     payload = {
-#         "jobType": "sync",
-#         "connectionId": os.getenv("airbyte_connection_id")
-#     }
-#     headers = {
-#         "accept": "application/json",
-#         "content-type": "application/json",
-#         "authorization": f"Bearer {token}"
-#     }
-#     response = requests.post(url, json=payload, headers=headers)
-#     print(response.text)
-
-
 def upload_json_files(**kwargs):
     """Uploads all JSON files in the current directory to the specified bucket.
 
@@ -290,21 +257,10 @@ with DAG(
     python_callable=fetch_and_save_velib_data,
     dag=dag)
 
-    # fetch_station_info = PythonOperator(
-    # task_id='fetch_station_info',
-    # python_callable=fetch_station_info,
-    # dag=dag)
-
     upload_to_bucket = PythonOperator(
     task_id='upload_to_bucket',
     python_callable=upload_json_files,
     dag=dag)
-
-    # trigger_airbyte = PythonOperator(
-    # task_id='trigger_airbyte',
-    # python_callable=trigger_airbyte,
-    # asynchronous=True,
-    # dag=dag)
 
     # In your DAG:
     trigger_airbyte_task = PythonOperator(
@@ -320,11 +276,28 @@ with DAG(
         provide_context=True,
         dag=dag,
     )
+
     dbt_run = BashOperator(
         task_id="dbt_run",
         bash_command=f"dbt run --project-dir {DBT_DIR}",
     dag=dag)
 
 
-    # fetch_spot_data >> upload_to_bucket >> trigger_airbyte >> wait_for_completion_task >> dbt_run
     fetch_spot_data >> upload_to_bucket >> trigger_airbyte_task >> wait_for_completion_task >> dbt_run
+
+
+
+with DAG(
+    "velib_dbt_run",
+    default_args=default_args,  # Use the consolidated default_args
+    description="DAG to call dbt run independently",
+    schedule_interval=None,
+    catchup=False,
+    start_date=pendulum.today("UTC")
+) as dbt_dag:
+    dbt_run = BashOperator(
+        task_id="dbt_run",
+        bash_command=f"dbt run --project-dir {DBT_DIR}",
+    dag=dbt_dag)
+
+    dbt_run
